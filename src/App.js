91e3198c33770daa658ab1563fa44fcfd094a381
sharske <sha-suke@ezweb.ne.jp>
2022-10-25 17:09:19 +0900

    イメージ作成

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { data } from "./weather";
+import { unstable_renderSubtreeIntoContainer } from "react-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="">
+      City Name
+      <p>{data[0].name}</p>
+      Weather Condition
+      <p>{data[0].weather[0].main}</p>
+      <div className="under">
+        <div className="Date">
+          <p>Data</p>
+          <p>
+            {new Date(data[0].dt * 1000)
+              .toLocaleDateString()
+              .split("/")
+              .join("-")}
+          </p>
+        </div>
+        <div className="Temprature">
+          <p>Temprature</p>
+          <p>{data[0].main.temp - 273}</p>
+        </div>
+        <div className="Humidity">
+          <p>Humidiy</p>
+          <p>{data[0].main.humidity}</p>
+        </div>
+      </div>
     </div>
   );
 }
