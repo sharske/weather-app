85b7112e20761e4f5674195a5bf712e27a0ea17d
sharske <sha-suke@ezweb.ne.jp>
2022-11-08 17:21:06 +0900

    feat: 天気のレイアウト作成

--- src/App.js
+++ src/App.js
@@ -2,31 +2,45 @@ import logo from "./logo.svg";
 import "./App.css";
 import { data } from "./weather";
 import { unstable_renderSubtreeIntoContainer } from "react-dom";
+import "./reset.css";
 
 function App() {
   return (
-    <div className="">
-      City Name
-      <p>{data[0].name}</p>
-      Weather Condition
-      <p>{data[0].weather[0].main}</p>
+    <div className="card">
+      <img src="/weather.png" alt="天気の画像" />
+      <p>City Name</p>
+      <div className="city">
+        <div className="date">
+          <p>{data[0].name}</p>
+        </div>
+      </div>
+      <p>Weather Condition</p>
+      <div className="date">
+        <p>{data[0].weather[0].main}</p>
+      </div>
       <div className="under">
         <div className="Date">
           <p>Data</p>
-          <p>
-            {new Date(data[0].dt * 1000)
-              .toLocaleDateString()
-              .split("/")
-              .join("-")}
-          </p>
+          <div className="date">
+            <p>
+              {new Date(data[0].dt * 1000)
+                .toLocaleDateString()
+                .split("/")
+                .join("-")}
+            </p>
+          </div>
         </div>
-        <div className="Temprature">
+        <div className="Temp">
           <p>Temprature</p>
-          <p>{data[0].main.temp - 273}</p>
+          <div className="date">
+            <p>{~~(data[0].main.temp - 273)}℃</p>
+          </div>
         </div>
         <div className="Humidity">
           <p>Humidiy</p>
-          <p>{data[0].main.humidity}</p>
+          <div className="date">
+            <p>{data[0].main.humidity}%</p>
+          </div>
         </div>
       </div>
     </div>
