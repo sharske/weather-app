73245914d3868dd66a1435cb8a597269999335e7
sharske <sha-suke@ezweb.ne.jp>
2022-11-15 14:31:11 +0900

    データをリストで表示

--- src/App.js
+++ src/App.js
@@ -6,44 +6,48 @@ import "./reset.css";
 
 function App() {
   return (
-    <div className="card">
-      <img src="/weather.png" alt="天気の画像" />
-      <p>City Name</p>
-      <div className="city">
-        <div className="date">
-          <p>{data[0].name}</p>
-        </div>
-      </div>
-      <p>Weather Condition</p>
-      <div className="date">
-        <p>{data[0].weather[0].main}</p>
-      </div>
-      <div className="under">
-        <div className="Date">
-          <p>Data</p>
+    <>
+      {data.map((item) => (
+        <div className="card">
+          <img src="/weather.png" alt="天気の画像" />
+          <p>City Name</p>
+          <div className="city">
+            <div className="date">
+              <p>{item.name}</p>
+            </div>
+          </div>
+          <p>Weather Condition</p>
           <div className="date">
-            <p>
-              {new Date(data[0].dt * 1000)
-                .toLocaleDateString()
-                .split("/")
-                .join("-")}
-            </p>
+            <p>{item.weather[0].main}</p>
+          </div>
+          <div className="under">
+            <div className="Date">
+              <p>Data</p>
+              <div className="date">
+                <p>
+                  {new Date(item.dt * 1000)
+                    .toLocaleDateString()
+                    .split("/")
+                    .join("-")}
+                </p>
+              </div>
+            </div>
+            <div className="Temp">
+              <p>Temprature</p>
+              <div className="date">
+                <p>{~~(item.main.temp - 273)}℃</p>
+              </div>
+            </div>
+            <div className="Humidity">
+              <p>Humidiy</p>
+              <div className="date">
+                <p>{item.main.humidity}%</p>
+              </div>
+            </div>
           </div>
         </div>
-        <div className="Temp">
-          <p>Temprature</p>
-          <div className="date">
-            <p>{~~(data[0].main.temp - 273)}℃</p>
-          </div>
-        </div>
-        <div className="Humidity">
-          <p>Humidiy</p>
-          <div className="date">
-            <p>{data[0].main.humidity}%</p>
-          </div>
-        </div>
-      </div>
-    </div>
+      ))}
+    </>
   );
 }
 
