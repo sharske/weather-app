61a1de9b37e5e94c98f6e2cd8927f553daf27b09
sharske <sha-suke@ezweb.ne.jp>
2022-11-15 17:29:00 +0900

    weather Appの改良

--- src/App.js
+++ src/App.js
@@ -1,14 +1,58 @@
-import logo from "./logo.svg";
 import "./App.css";
-import { data } from "./weather";
-import { unstable_renderSubtreeIntoContainer } from "react-dom";
+// import { data } from "./weather";
 import "./reset.css";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [cityNames, setCityNames] = useState();
+  const [selectedCityNames, setSelectedCityNames] = useState("tokyo");
+  const [res, setRes] = useState();
+
+  useEffect(() => {
+    async function f() {
+      const result1 = await fetch("http://localhost:8000/weather/city_names");
+      setCityNames(await result1.json());
+    }
+    f();
+  }, []);
+
+  useEffect(() => {
+    async function f() {
+      const result2 = await fetch(
+        "http://localhost:8000/weather/" + selectedCityNames
+      );
+      setRes([await result2.json()]);
+    }
+    f();
+  }, [selectedCityNames]);
+
   return (
     <>
-      {data.map((item) => (
-        <div className="card">
+      {/* <div style={{ display: "flex", flexDirection: "column", gap: 30 }}>
+        {res?.map((item) => (
+          <div
+            style={{
+              width: 400,
+              height: 100,
+              background: "green",
+              borderRadius: 30,
+              padding: 20,
+            }}
+          >
+            <div>id: {item.id}</div>
+            <div>title: {item.title}</div>
+          </div>
+        ))}
+      </div> */}
+      <select onChange={(e) => setSelectedCityNames(e.target.value)}>
+        {cityNames?.map((cityName) => (
+          <option value={cityName}>{cityName}</option>
+        ))}
+      </select>
+
+      {/* <div>{JSON.stringify(cityNames)}</div> */}
+      {res?.map((item) => (
+        <div key={item.id} className="card">
           <img src="/weather.png" alt="天気の画像" />
           <p>City Name</p>
           <div className="city">
